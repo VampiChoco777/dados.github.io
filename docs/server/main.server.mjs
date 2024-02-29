5d55c7ad7dd4714dcf20ba21e5865129aca03824
Marco Adrián Domínguez Jiménez <l20140892@queretaro.tecnm.mx>
2024-02-29 10:43:39 -0600

    first commit

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-HEPM442N.mjs";import"./chunk-2K3IABWX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dados';
+  dadoIzquierdo = '../assets/img/dice_1.png';
+  dadoDerecho = '../assets/img/dice_4.png';
+  numero1: number = 0;
+  numero2: number = 0;
+  tirarDados(): void
+  {
+    this.numero1=Math.round(Math.random()*5)+1;
+    this.numero2=Math.round(Math.random()*5)+1;
+    this.dadoIzquierdo = '../assets/img/dice_'+this.numero1+'.png';
+    this.dadoDerecho = '../assets/img/dice_'+this.numero2+'.png';
+  }
 }
